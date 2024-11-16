9cae882e95e5dbfda343de3cafb4e87ebfa240ae
Jayamuni Rashminda <rashminda121@gmail.com>
2024-11-16 22:07:35 +0530

    updated backend files

--- backend/src/main/java/com/EmployeeSys/employee/controller/EmployeeController.java
+++ backend/src/main/java/com/EmployeeSys/employee/controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package com.EmployeeSys.employee.controller;
 
+import com.EmployeeSys.employee.entity.Employee;
+import com.EmployeeSys.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    @PostMapping("/employee")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeService.postEmployee(employee);
+
+    }
 }
